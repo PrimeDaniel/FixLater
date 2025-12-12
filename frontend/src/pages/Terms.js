1061e8fa67bac70c3090b57c4e9fe465cbeab29c
Daniel Fraimovich <128545450+PrimeDaniel@users.noreply.github.com>
2025-12-12 14:08:26 +0000

    feat: add Terms of Service page

---
+++ frontend/src/pages/Terms.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const Terms = () => (
+  <Container maxWidth="md" sx={{ py: 4 }}>
+    <Typography variant="h3" gutterBottom>Terms of Service</Typography>
+    <Typography variant="body1" color="text.secondary">
+      By using FixLater, you agree to our terms and conditions. (This is a placeholder page.)
+    </Typography>
+  </Container>
+);
+
+export default Terms;
